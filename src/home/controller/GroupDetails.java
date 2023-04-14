803c00ad7fb7859f51288490320ea36fe4af0dee
Farida Fakhri <fakhri.f@northeastern.edu>
2023-04-13 22:46:15 -0400

    Splitwise Functionality

--- src/home/controller/GroupDetails.java
+++ src/home/controller/GroupDetails.java
@@ -1,5 +1,99 @@
 package home.controller;
 
-public class GroupDetails {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+import java.util.*;
+
+public class GroupDetails {
+    public String groupName;
+    public List<String> members;
+    public List<String> transactions;
+    private String userName = "farida";
+    
+    @FXML
+    public Label groupNameLabel;
+    
+    @FXML
+    public VBox balancesVBox;
+    
+    @FXML
+    private ListView<String> listOfTransactionNames;
+    
+    @FXML
+    void initialize() {
+    	// Set the group name label here
+    	groupNameLabel.setText(this.groupName);
+    	
+    	// Get group members names
+    	getMemberNames();
+    	System.out.println(this.members);
+    	
+    	// display member names in vBox
+    	renderMemberNames();
+    	getTransactionNames();
+    	renderTransactionNames();
+    }
+    
+    void getMemberNames() {
+    	members = new ArrayList<String>();
+		try (Connection connection = DatabaseConnection.Connect()){
+	        String memberQuery = "SELECT memberName FROM all_groups WHERE groupName = '" + this.groupName + "'";
+	        PreparedStatement memberStatement = connection.prepareStatement(memberQuery);
+	        ResultSet rs = memberStatement.executeQuery();
+	        while(rs.next()) {
+	        	this.members.add(rs.getString(1));
+	        }
+		} catch (SQLException e) {
+		    System.out.println(e.getMessage());
+		}
+    }
+    
+    void getTransactionNames() {
+    	transactions = new ArrayList<String>();
+		try (Connection connection = DatabaseConnection.Connect()){
+	        String transactionQuery = "SELECT distinct t.id, t.transactionName FROM transaction t JOIN transaction_split ts ON t.id = ts.transactionId WHERE t.payerName = '" + userName + "' OR ts.payeeName = '" + userName + "' AND t.groupName = '" + groupName + "'";
+	        PreparedStatement transactionStatement = connection.prepareStatement(transactionQuery);
+	        System.out.println(transactionStatement.toString());
+	        ResultSet rs = transactionStatement.executeQuery();
+	        while(rs.next()) {
+	        	this.transactions.add(rs.getString(2));
+	        }
+	        System.out.println(transactions);
+		} catch (SQLException e) {
+		    System.out.println(e.getMessage());
+		}
+    }
+    
+    
+    @FXML
+    void renderTransactionNames() {
+    	System.out.println("Adding element to " + listOfTransactionNames.getItems());
+    	for(String name : transactions) 
+    	{
+    		listOfTransactionNames.getItems().add(name);
+    	}
+  
+    }
+    
+    void renderMemberNames() {
+    	for(String memberName : members) {
+    		Label memberLabel = new Label(memberName);
+    		balancesVBox.getChildren().add(memberLabel);
+    	}
+    }
+    
+    public void setGroupName(String groupName) {
+    	this.groupName = groupName;
+    }
 }
--- src/home/controller/SplitBillCreateGroups.java
+++ src/home/controller/SplitBillCreateGroups.java
@@ -3,19 +3,33 @@ package home.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import java.util.*;
+
 public class SplitBillCreateGroups {
+	private List<String> groupNames;
+	// to be deleted
+	private String  username = "farida";
 
     @FXML
     private TextField groupName;
@@ -25,11 +39,85 @@ public class SplitBillCreateGroups {
 
     @FXML
     private TextField name;
+    
+    @FXML
+    private VBox groupListVbox;
 
     private Parent root;
     
     @FXML
+    void initialize() {
+    	// Get list of group names
+    	getGroupNames();
+    	
+    	// Populate view with group names and buttons
+    	renderGroupNames();
+    }
+    
+    void getGroupNames() {
+    	groupNames = new ArrayList<String>();
+		try (Connection connection = DatabaseConnection.Connect()){
+	        String groupQuery = "SELECT DISTINCT groupName FROM all_groups WHERE memberName = ?";
+	        PreparedStatement groupStatement = connection.prepareStatement(groupQuery);
+	        groupStatement.setString(1, username);
+	        ResultSet rs = groupStatement.executeQuery();
+	        while(rs.next()) {
+	        	this.groupNames.add(rs.getString(1));
+	        }
+		} catch (SQLException e) {
+		    System.out.println(e.getMessage());
+		}
+    }
+    
+    void renderGroupNames() {
+    	groupListVbox.getChildren().clear();
+    	for(String groupName : groupNames) {
+    		HBox hbox = new HBox(20);
+    		Label groupLabel = new Label(groupName);
+    		groupLabel.setPrefWidth(120); 
+    	    groupLabel.setPrefHeight(10);
+    	    //groupLabel.setStyle("-fx-padding: 0 50 0 0;"); // add right padding to the label
+    	      
+    		Button viewGroupButton = new Button("View Group");
+    		viewGroupButton.setPrefWidth(120); 
+    	    viewGroupButton.setPrefHeight(10);
+    	    viewGroupButton.setStyle("-fx-background-color: #ADD8E6;");
+    		
+    		viewGroupButton.setOnAction(e -> openGroupView(e, groupName));
+    		hbox.getChildren().addAll(groupLabel, viewGroupButton);
+    		//HBox.setMargin(groupLabel, new Insets(0, 0, 0, 10));
+    		groupListVbox.getChildren().add(hbox);
+    	}
+    }
+    
+    void openGroupView(ActionEvent event, String groupName) {
+    	System.out.println(groupName);
+        System.out.println("View Group Clicked!");
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        stage.close();
+        try {
+       	 URL url = new File("src/home/fxml/GroupDetails.fxml").toURI().toURL();
+      
+       	 FXMLLoader loader = new FXMLLoader(url);
+       	 
+       	 GroupDetails controller = new GroupDetails();
+       	 controller.setGroupName(groupName);
+       	 
+       	 loader.setController(controller);
+       	 
+       	 Parent root = loader.load();
+       	 Scene scene = new Scene(root);
+       	 stage.setScene(scene);
+       	 stage.show();
+        } catch (IOException e) {
+       	 System.err.println(String.format("Error: %s", e.getMessage()));
+        }
+    }
+    
+    @FXML
     void addName(MouseEvent event) {
+    	System.out.println("Adding element to " + listOfNames.getItems());
     	listOfNames.getItems().add(name.getText());
     }
 
@@ -39,30 +127,52 @@ public class SplitBillCreateGroups {
     	listOfNames.getItems().remove(selectedID);
 
     }
-    
+  
     @FXML
-    void viewGroup(MouseEvent event) throws IOException {
-         FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/GroupDetails.fxml"));
-         root = loader.load();
-         GroupDetails groupDetails = loader.getController();
-         
-         
+    void viewGroup(MouseEvent event) {         
+         System.out.println("View Group Clicked!");
+         Node node = (Node) event.getSource();
+         Stage stage = (Stage) node.getScene().getWindow();
+         stage.close();
+         try {
+        	 URL url = new File("src/home/fxml/GroupDetails.fxml").toURI().toURL();
+       
+        	 FXMLLoader loader = new FXMLLoader(url);
+        	 
+        	 GroupDetails controller = new GroupDetails();
+        	 controller.setGroupName("dummyGroup");
+        	 
+        	 loader.setController(controller);
+        	 
+        	 Parent root = loader.load();
+        	 Scene scene = new Scene(root);
+        	 stage.setScene(scene);
+        	 stage.show();
+         } catch (IOException e) {
+        	 System.err.println(String.format("Error: %s", e.getMessage()));
+         }
     }
     
     @FXML
-    void saveData(ActionEvent event) {
-         try (Connection connection = DatabaseConnection.Connect()){
-    		for (String memberName : listOfNames.getItems()) {
-                String memberQuery = "INSERT INTO all_groups (groupName, memberName) VALUES (?, ?)";
-                PreparedStatement memberStatement = connection.prepareStatement(memberQuery);
-                memberStatement.setString(1, groupName.getText());
-                memberStatement.setString(2, memberName);
-                memberStatement.executeUpdate();
-            }
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+    void saveData(MouseEvent event) {
+    	System.out.println("Save Data Invoked!");
+		try (Connection connection = DatabaseConnection.Connect()){
+			for (String memberName : listOfNames.getItems()) {
+		        String memberQuery = "INSERT INTO all_groups (groupName, memberName) VALUES (?, ?)";
+		        PreparedStatement memberStatement = connection.prepareStatement(memberQuery);
+		        memberStatement.setString(1, groupName.getText());
+		        memberStatement.setString(2, memberName);
+		        memberStatement.executeUpdate();
+		    }
+			// Get list of group names
+	    	getGroupNames();
+	    	
+	    	// Populate view with group names and buttons
+	    	renderGroupNames();
+		
+		} catch (SQLException e) {
+		    System.out.println(e.getMessage());
+		}
     }
  }
 
